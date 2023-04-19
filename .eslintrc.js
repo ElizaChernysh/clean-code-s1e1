a10e091686abd9958431a5809d735ca4a34fa155
Eliza Chernysh <82468286+ElizaChernysh@users.noreply.github.com>
2023-04-19 19:17:49 +0300

    docs: add stylelint config (Wed Apr 19, 2023  19:17:49PM)

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,18 +5,30 @@ module.exports = {
     'es2021': true,
     'es6': true
   },
-  'plugins': ['@html-eslint'],
+  'plugins': ['@html-eslint', 'css'],
   'overrides': [
     {
-      files: ['*.html'],
+      files: ['*.html', ],
       parser: '@html-eslint/parser',
       extends: ['plugin:@html-eslint/recommended', 'prettier'],
     },
+    {
+      files: ['*.css'],
+      extends: ["stylelint-config-standard"],
+      rules: {
+        "css/color-hex-case": ["error", "lower"]
+      }
+    }
   ],
   'parserOptions': {
     'ecmaVersion': 'latest'
   },
   'rules': {
-    'quotes': [2, 'single', { 'avoidEscape': true }]
+    // 'quotes': [2, 'single', { 'avoidEscape': true }],
+    "css/number-leading-zero": ["error",
+      "always"],
+    "css/color-hex-style": ["error",
+      "RGB", // "RRGGBB"
+    ]
   }
 }
